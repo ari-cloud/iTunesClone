6fc89363f5d4193341da674b99f63680ac0d2a4d
Arina <karma.rin512@gmail.com>
2023-07-16 20:55:29 +0700

    first commit

---
+++ iTunes/UI/Modules/HomeScreen/HomeScreenViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class HomeScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ iTunes/UI/Modules/HomeScreen/HomeScreenViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeScreenViewModel.swift
+//  iTunes
+//
+//  Created by Арина Моргачева on 16.07.2023.
+//
+
+import Foundation
